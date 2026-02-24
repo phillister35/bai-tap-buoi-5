0ffa3f71869abe3ba0f2c5731c98a8daa3c13c7a
phillister35 <philippe.truong.dang@gmail.com>
2026-02-24 20:43:57 +0700

    2nd commitment

---
+++ đếm giờ.js
@@ -0,0 +1,12 @@
+let time = 10;
+
+while (time !== 0) {
+  if (time === 5) {
+    console.log("đã quá nửa thời gian");
+  } else {
+    console.log("còn lại " + time + " giây");
+  }
+  time--;
+}
+
+console.log("hết giờ");
